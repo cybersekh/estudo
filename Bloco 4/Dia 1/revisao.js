1ba09a5f477aa81cb7c0efea3d8efea1865aa23a
Carol <carolina.jpeg@gmail.com>
2022-06-26 18:29:28 -0300

    kkkkkk estudando

---
+++ Bloco 4/Dia 1/revisao.js
@@ -0,0 +1,49 @@
+// let idade = 18;
+
+// switch (idade) {
+//     case 18:
+//     console.log('mais novo que Carol');
+//     break;
+//     case 27:
+//     console.log('idade da Carol');
+//     break;
+//     case 28:
+//     console.log('mais velho que Carol');
+//     break;
+
+//     default:
+//     console.log('Suas opcoes sao 18, 27 e 28');
+//     break;
+// }
+
+// for (elemento of arrayzin) {
+//     elemento
+// }   PEGA O NEGOCIO EM SI, VALOR
+ 
+// let nomes = ['Carol', 'Vinicius', 'Isa', 'Arthur'];
+
+// let elemento
+// for (elemento in nomes) {
+//     console.log(elemento)
+// } PEGA O INDEX, POSICAO OU A CHAVE (exemplo {nome: carolina}, pegaria a chave 'nome')
+
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for (let person in names) {
+//     console.log('Olá ' + names[person])
+//   }
+
+//   let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+
+//   for (let info in car) {
+//     console.log(info, car[info])
+//   }
